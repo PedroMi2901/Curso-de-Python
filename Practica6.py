50db9e6913fa4ca372870df1cce7a4fc9038e71b
PedroMi2901 <64527357+PedroMi2901@users.noreply.github.com>
2020-04-30 13:12:44 +0200

    Práctica del 6 al 9

---
+++ Practica6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 12:51:56 2020
+
+@author: pedro
+"""
+
+abecedario=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print("BIENVENIDO A MI CIFRADOR CÉSAR")
+
+texto_claro=input("Escribe el texto a cifrar:")
+clave=int(input("Escribe la clave de cifrado (un número del 1 al 27):"))
+
+texto_cifrado=""
+
+for letra in texto_claro:
+    nueva_posicion=(abecedario.index(letra)+clave) % 27
+    letra_cifrada=abecedario[nueva_posicion]
+    texto_cifrado+=letra_cifrada
+
+print(texto_cifrado)
